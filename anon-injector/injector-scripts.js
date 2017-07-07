5749422e0304900ce4785c34226e6edfa3c72f6c
CorentinAddventa <corentin.gaillard@addventa.com>
2017-07-07 10:28:34 +0200

    request

--- anon-injector/injector-scripts.js
+++ anon-injector/injector-scripts.js
@@ -559,7 +559,15 @@ function generateResult() {
 			url : action,
 			headers: {
     			"Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
-  			},
+  			}
+  			method : 'POST',
+			dataType : "html",
+			async: false,
+			contentType : "application/x-www-form-urlencoded; charset=iso-8859-1"}
+  		});
+		$
+		.ajax({
+			url : action,
 			data : {
 				xml : xmlInput
 			},
